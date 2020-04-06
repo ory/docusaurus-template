5cc2f31fed357d952b5b3cf4eb0ef6e3ccf87e14
aeneasr <aeneas@ory.sh>
2020-04-06 18:50:37 +0200

    u

--- update.sh
+++ update.sh
@@ -2,20 +2,25 @@
 
 set -euox pipefail
 
+rm -rf .docusaurus package-lock.json build || true
+npm i
+rm -rf node_modules || true
+
 function update {
     dir="$(mktemp -d -t ci-XXXXXXXXXX)/$1"
     git clone git@github.com:ory/"$1".git "$dir" || true
-
-    rm -rf node_modules .docusaurus package-lock.json build || true
-    cp -R ../docusaurus-template/. "$dir/docs"
 
     (cd "$dir"; \
       git town main-branch master; \
       git checkout master; \
       git reset --hard HEAD; \
-      git hack docusaurus-$(date +"%m-%d-%y-%H-%M-%S"); \
-      (cd docs; npm install); \
-      rm docs/update.sh;
+      git hack docusaurus-$(date +"%m-%d-%y-%H-%M-%S"))
+
+    cp -Rf ../docusaurus-template/. "$dir/docs"
+
+    (cd "$dir"; \
+      rm docs/update.sh; \
+      git status; \
       (git add -A && \
       git commit -a -s -m "chore: update docusaurus template" && \
       git npr) || true
