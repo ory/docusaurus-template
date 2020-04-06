9a3278bf653c19d69dd70df678daadd345a059ca
hackerman <3372410+aeneasr@users.noreply.github.com>
2020-04-06 19:36:25 +0200

    feat: implement docusaurus v2 template (#1)

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euox pipefail
+
+rm -rf .docusaurus package-lock.json build || true
+npm i
+rm -rf node_modules || true
+
+function update {
+    dir="$(mktemp -d -t ci-XXXXXXXXXX)/$1"
+    git clone git@github.com:ory/"$1".git "$dir" || true
+
+    (cd "$dir"; \
+      git town main-branch master; \
+      git checkout master; \
+      git reset --hard HEAD; \
+      git hack docusaurus-$(date +"%m-%d-%y-%H-%M-%S"))
+
+    cp -Rf ../docusaurus-template/. "$dir/docs"
+
+    (cd "$dir"; \
+      rm docs/update.sh; \
+      git status; \
+      (git add -A && \
+      git commit -a -s -m "chore: update docusaurus template" && \
+      git npr) || true
+    )
+}
+
+update oathkeeper
+update keto
+update hydra
+update kratos
