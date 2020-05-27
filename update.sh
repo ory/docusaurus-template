cafa6f5680fd0d610a40611ffecee1bbc5943368
Patrik <zepatrik@users.noreply.github.com>
2020-05-27 17:40:19 +0200

    feat: make circle ci update from master automatically (#11)

--- update.sh
+++ update.sh
@@ -6,15 +6,25 @@ rm -rf .docusaurus package-lock.json build || true
 npm i
 rm -rf node_modules || true
 
+# get github api client
+wget https://github.com/cli/cli/releases/download/v0.9.0/gh_0.9.0_linux_amd64.tar.gz
+tar -xf gh_0.9.0_linux_amd64.tar.gz
+mv gh_0.9.0_linux_amd64/bin/gh gh
+
+# get git-town
+wget -O git-town https://github.com/git-town/git-town/releases/download/v7.3.0/git-town-linux-amd64
+chmod +x ./git-town
+
 function update {
     dir="$(mktemp -d -t ci-XXXXXXXXXX)/$1"
+    branch="docusaurus-$(date +\"%m-%d-%y-%H-%M-%S\")"
     git clone git@github.com:ory/"$1".git "$dir" || true
 
     (cd "$dir"; \
-      git town main-branch master; \
+      ./git-town main-branch master; \
       git checkout master; \
       git reset --hard HEAD; \
-      git hack docusaurus-$(date +"%m-%d-%y-%H-%M-%S"))
+      ./git-town hack $branch
 
     cp -Rf ../docusaurus-template/. "$dir/docs"
     mv -f "$dir/docs/README_TEMPLATE.md" "$dir/docs/README.md"
@@ -24,7 +34,8 @@ function update {
       git status; \
       (git add -A && \
       git commit -a -s -m "chore: update docusaurus template" && \
-      git npr) || true
+      git push --set-upstream origin $branch && \
+      ./gh pr create --title "chore: update docusaurus template" --body "Updated docusaurus template to current https://github.com/ory/docusaurus-template.") || true
     )
 }
 
@@ -32,7 +43,4 @@ update oathkeeper
 update keto
 update hydra
 update kratos
-
-if [ "$1" = "--private" ]; then
-  update cloud
-fi
+update cloud
