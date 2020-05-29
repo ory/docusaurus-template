d23d83cf3bf01188461dd958251830181d17e0c8
zepatrik <patrik@ory.sh>
2020-05-29 11:38:12 +0200

    fix: change relative path to copy the template in update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ function update {
       git reset --hard HEAD; \
       git-town hack $branch)
 
-    cp -Rf ../docusaurus-template/. "$dir/docs"
+    cp -Rf ./* "$dir/docs"
 
     (cd "$dir"; \
       rm docs/update.sh; \
