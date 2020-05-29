c56c4697950638f665c97ed6c0bfb44f125b1ff1
zepatrik <patrik@ory.sh>
2020-05-29 10:17:12 +0200

    fix: add binaries to path

--- update.sh
+++ update.sh
@@ -6,6 +6,10 @@ rm -rf .docusaurus package-lock.json build || true
 npm i
 rm -rf node_modules || true
 
+mkdir bin
+export PATH="$PATH:$(pwd)/bin"
+cd bin
+
 # get github api client
 wget https://github.com/cli/cli/releases/download/v0.9.0/gh_0.9.0_linux_amd64.tar.gz
 tar -xf gh_0.9.0_linux_amd64.tar.gz
@@ -21,21 +25,22 @@ function update {
     git clone git@github.com:ory/"$1".git "$dir" || true
 
     (cd "$dir"; \
-      ./git-town main-branch master; \
+      git-town main-branch master; \
       git checkout master; \
       git reset --hard HEAD; \
-      ./git-town hack $branch)
+      git-town hack $branch)
 
     cp -Rf ../docusaurus-template/. "$dir/docs"
-    mv -f "$dir/docs/README_TEMPLATE.md" "$dir/docs/README.md"
 
     (cd "$dir"; \
       rm docs/update.sh; \
+      rm -rfd docs/bin; \
+      mv -f docs/README_TEMPLATE.md docs/README.md; \
       git status; \
       (git add -A && \
       git commit -a -s -m "chore: update docusaurus template" && \
       git push --set-upstream origin $branch && \
-      ./gh pr create --title "chore: update docusaurus template" --body "Updated docusaurus template to current https://github.com/ory/docusaurus-template.") || true
+      gh pr create --title "chore: update docusaurus template" --body "Updated docusaurus template to current https://github.com/ory/docusaurus-template.") || true
     )
 }
 
