b7e7053dcbe3a888358e743c312b9c33fc6543c0
Patrik <zepatrik@users.noreply.github.com>
2020-05-29 13:07:45 +0200

    fix: update.sh working (#24)

--- update.sh
+++ update.sh
@@ -2,23 +2,21 @@
 
 set -euox pipefail
 
+
+bin=$(mktemp -d -t bin-XXXXXX)
+export PATH="$PATH:$bin"
+
 rm -rf .docusaurus package-lock.json build || true
 npm i
-rm -rf node_modules || true
-
-
-mkdir bin
-export PATH="$PATH:$(pwd)/bin"
-cd bin
 
 # get github api client
-wget https://github.com/cli/cli/releases/download/v0.9.0/gh_0.9.0_linux_amd64.tar.gz
-tar -xf gh_0.9.0_linux_amd64.tar.gz
-mv gh_0.9.0_linux_amd64/bin/gh gh
+wget -O "$bin/gh.tar.gz" https://github.com/cli/cli/releases/download/v0.9.0/gh_0.9.0_linux_amd64.tar.gz
+tar -xf "$bin/gh.tar.gz"
+mv gh_0.9.0_linux_amd64/bin/gh "$bin/gh"
 
 # get git-town
-wget -O git-town https://github.com/git-town/git-town/releases/download/v7.3.0/git-town-linux-amd64
-chmod +x ./git-town
+wget -O "$bin/git-town" https://github.com/git-town/git-town/releases/download/v7.3.0/git-town-linux-amd64
+chmod +x "$bin/git-town"
 
 function update {
     dir="$(mktemp -d -t ci-XXXXXXXXXX)/$1"
@@ -35,8 +33,8 @@ function update {
 
     (cd "$dir"; \
       rm docs/update.sh; \
-      rm -rfd docs/bin; \
       mv -f docs/README_TEMPLATE.md docs/README.md; \
+      rm -rf docs/node_modules docs/.circleci || true ; \
       git status; \
       (git add -A && \
       git commit -a -s -m "chore: update docusaurus template" && \
