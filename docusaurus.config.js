37ceab0cbeea22451e4b2fd4a39a2174269be231
jfcurran <john@ory.sh>
2021-02-16 19:30:37 +0100

    fix: run format

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,10 @@ module.exports = {
         alt: config.projectName,
         src: `img/logo-${config.projectSlug}.svg`,
         srcDark: `img/logo-${config.projectSlug}.svg`,
-        href: config.projectSlug === 'docs' ? `https://www.ory.sh` : `https://www.ory.sh/${config.projectSlug}`,
+        href:
+          config.projectSlug === 'docs'
+            ? `https://www.ory.sh`
+            : `https://www.ory.sh/${config.projectSlug}`
       },
       items: [
         ...links,
